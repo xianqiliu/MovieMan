f81cc4a677c2e8373875d24ab7e50ea1456d6599
kyleliuxianqi <liuxianqi1997@qq.com>
2021-11-24 09:26:07 +0100

    Initial commit

---
+++ app/src/main/java/fr/isep/ii3510/movieman/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.isep.ii3510.movieman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
